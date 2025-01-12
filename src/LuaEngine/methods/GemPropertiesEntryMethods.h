b8bbed64fc3bc365d4ee0e5eb083f054407d1659
iThorgrim <contact@warlog.dev>
2025-01-12 15:09:07 +0100

    Refactor(Methods/GemPropertiesMethods): Rename files and add new method

--- src/LuaEngine/methods/GemPropertiesEntryMethods.h
+++ src/LuaEngine/methods/GemPropertiesEntryMethods.h
@@ -4,17 +4,16 @@
 * Please see the included DOCS/LICENSE.md for more information
 */
 
-#ifndef GEMPROPERTIESMETHODS_H
-#define GEMPROPERTIESMETHODS_H
+#ifndef GEMPROPERTIESENTRYMETHODS_H
+#define GEMPROPERTIESENTRYMETHODS_H
 
-namespace LuaGemProperties
+namespace LuaGemPropertiesEntry
 {
 
     /**
      * Returns the ID of a [GemPropertiesEntry].
      *
-     * This method retrieves the ID from a given GemPropertiesEntry instance 
-     * and pushes it onto the Lua stack.
+     * This method retrieves the ID from a given GemPropertiesEntry.
      *
      * @return uint32 id : The ID of the specified GemPropertiesEntry.
      */
@@ -25,10 +24,24 @@ namespace LuaGemProperties
     }
 
 
+    /**
+     * Returns the spell item enchantment of a [GemPropertiesEntry].
+     *
+     * This function retrieves the `spellitemenchantement` attribute from the provided `GemPropertiesEntry`.
+     *
+     * @return uint32 spellitemenchantement : The spell item enchantment ID.
+     */
+    int GetSpellItemEnchantement(lua_State* L, GemPropertiesEntry* entry)
+    {
+        Forge::Push(L, entry->spellitemenchantement);
+        return 1;
+    }
+
     ElunaRegister<GemPropertiesEntry> GemPropertiesMethods[] =
     {
         // Getters
         { "GetID", &LuaGemProperties::GetID },
+        { "GetSpellItemEnchantement", &LuaGemPropertiesEntry::GetSpellItemEnchantement },
 
         // Setters
 
